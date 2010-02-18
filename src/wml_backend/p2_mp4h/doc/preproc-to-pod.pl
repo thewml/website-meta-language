900dd4750c5e5ef72e00f1fb7c19c9331ecd60a2
shlomif <shlomif@7081e830-ea04-0410-ae62-fb6148e2d9db>
2010-02-18 11:45:32 +0000

    More install progress

---
+++ src/wml_backend/p2_mp4h/doc/preproc-to-pod.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+my $mp4h;
+my $srcdir;
+my $input_fn;
+my $output_fn;
+
+GetOptions(
+    "mp4h=s" => \$mp4h,
+    "srcdir=s" => \$srcdir,
+    "input=s" => \$input_fn,
+    "output=s" => \$output_fn,
+);
+
+my $flags = qq{-X 0 -I $srcdir -D srcdir="$srcdir"};
+
+open my $mp4h_fh, "$mp4h $flags -D format=pod $input_fn |"
+    or die "Could not open mp4h!";
+
+open my $out_fh, ">", $output_fn
+    or die "Could not open output filename for writing - '$output_fn'";
+
+while (<$mp4h_fh>)
+{
+    s/_LT_/</g;
+    s/_GT_/>/g;
+    print {$out_fh} $_;
+}
+
+close($out_fh);
+close($mp4h_fh);
+
