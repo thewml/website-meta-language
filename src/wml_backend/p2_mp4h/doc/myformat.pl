83dfb9645c6b6ea7619d994c681107ea2504e847
Shlomi Fish <shlomif@shlomifish.org>
2012-06-01 19:57:34 +0300

    Add the p2_mp4h/doc dir.
    Some stuff was not yet imported from the Makefile.am.

---
+++ src/wml_backend/p2_mp4h/doc/myformat.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings; 
+
+my ($MYPROGRAM, $MYFLAGS, $FORMAT, $CMAKE_CURRENT_SOURCE_DIR, $TARGET) = @ARGV;
+my $out = `${MYPROGRAM} ${MYFLAGS} -D format=${FORMAT} ${CMAKE_CURRENT_SOURCE_DIR}/mp4h.mp4h`; 
+
+# print STDERR  "FOOBARBAZQUUX === ${MYPROGRAM} ${MYFLAGS} -D format=${FORMAT} ${CMAKE_CURRENT_SOURCE_DIR}/mp4h.mp4h";
+
+$out =~ s/_LT_/</g; 
+$out =~ s/_GT_/>/g; 
+
+open my $fh, '>', $TARGET or die qq/Cannot open ${TARGET} - $!/ ;
+print {$fh} $out;
+close($fh);
