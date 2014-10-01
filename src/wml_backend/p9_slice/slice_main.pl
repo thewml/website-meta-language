d41a1cee13a3b11fa03567efc847a4dbfa471208
Shlomi Fish <shlomif@shlomifish.org>
2014-10-01 17:42:49 +0300

    Add strict+warnings

--- src/wml_backend/p9_slice/slice_main.pl
+++ src/wml_backend/p9_slice/slice_main.pl
@@ -2,6 +2,9 @@
 eval 'exec @PATH_PERL@ -S $0 ${1+"$@"}'
     if $running_under_some_shell;
 
+use strict;
+use warnings;
+
 #   bootstrapping private installed modules
 use lib '@INSTALLPRIVLIB@';
 use lib '@INSTALLARCHLIB@';
