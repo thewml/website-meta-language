c58720655e9a3b462f6ae78a2ccaf98d599a48bc
Shlomi Fish <shlomif@shlomifish.org>
2018-04-17 20:57:03 +0300

    Remove old autoconf files.

--- src/config/prep_modules_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-myfile=../wml_backend/p2_mp4h/modules/subdirs.am
-if test -n "$CYGWIN" ; then
-    echo "SUBDIRS = " > "$myfile"
-else
-    echo "SUBDIRS = intl system" > "$myfile"
-fi
