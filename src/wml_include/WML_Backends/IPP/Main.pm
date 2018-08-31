854c20a11c5357d0e68868e91909963fa20cbc60
Shlomi Fish <shlomif@shlomifish.org>
2018-08-31 22:15:22 +0300

    Extract a method or a function.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- src/wml_include/WML_Backends/IPP/Main.pm
+++ src/wml_include/WML_Backends/IPP/Main.pm
@@ -101,6 +101,65 @@ EOF
     exit(1);
 }
 
+sub _PatternProcess_helper
+{
+    my (
+        $self,    $test, $out,   $mode,  $_del, $dirname,
+        $pattern, $ext,  $level, $no_id, $arg
+    ) = @_;
+    if ( not -d $dirname )
+    {
+        return;
+    }
+    my @ls =
+        grep { /^$pattern$/ && $test->('.') } @{ io->dir("$dirname") };
+
+    #   Sort list of files
+    my $criterion = $arg->{'IPP_SORT'} || $arg->{'IPP_REVERSE'};
+    if ( $criterion eq 'date' )
+    {
+        @ls = sort { -M $a <=> -M $b } @ls;
+    }
+    elsif ( $criterion eq 'numeric' )
+    {
+        @ls = sort { $a <=> $b } @ls;
+    }
+    elsif ($criterion)
+    {
+        @ls = sort @ls;
+    }
+    @ls = reverse @ls if ( $arg->{'IPP_REVERSE'} );
+
+    #   and truncate it
+    if ( $arg->{'IPP_MAX'} =~ m/^\d+$/ and $arg->{'IPP_MAX'} < @ls )
+    {
+        splice( @ls, $arg->{'IPP_MAX'} - scalar(@ls) );
+    }
+    push( @ls, "" );
+
+    $arg->{'IPP_NEXT'} = '';
+    $arg->{'IPP_THIS'} = '';
+LS:
+    foreach (@ls)
+    {
+        next LS if ( m|/\.+$| or m|^\.+$| );
+
+        #   set IPP_PREV, IPP_THIS, IPP_NEXT
+        $arg->{'IPP_PREV'} = $arg->{'IPP_THIS'};
+        $arg->{'IPP_THIS'} = $arg->{'IPP_NEXT'};
+        $arg->{'IPP_NEXT'} = ( $_ eq '' ? '' : "$dirname/$_$ext" );
+        next LS if $arg->{'IPP_THIS'} eq '';
+
+        $$out .=
+            $self->ProcessFile( $mode, $_del, $arg->{'IPP_THIS'}, "",
+            $level, $no_id, $arg );
+    }
+    delete $arg->{'IPP_NEXT'};
+    delete $arg->{'IPP_THIS'};
+    delete $arg->{'IPP_PREV'};
+    return;
+}
+
 sub PatternProcess
 {
     my ( $self, $mode, $_del, $dirname, $pattern, $ext, $level, $no_id, $arg )
@@ -147,55 +206,10 @@ sub PatternProcess
     }
     if ( $_del->is_quote_all )
     {
-        if ( -d $dirname )
-        {
-            my @ls =
-                grep { /^$pattern$/ && $test->('.') } @{ io->dir("$dirname") };
-
-            #   Sort list of files
-            my $criterion = $arg->{'IPP_SORT'} || $arg->{'IPP_REVERSE'};
-            if ( $criterion eq 'date' )
-            {
-                @ls = sort { -M $a <=> -M $b } @ls;
-            }
-            elsif ( $criterion eq 'numeric' )
-            {
-                @ls = sort { $a <=> $b } @ls;
-            }
-            elsif ($criterion)
-            {
-                @ls = sort @ls;
-            }
-            @ls = reverse @ls if ( $arg->{'IPP_REVERSE'} );
-
-            #   and truncate it
-            if ( $arg->{'IPP_MAX'} =~ m/^\d+$/ and $arg->{'IPP_MAX'} < @ls )
-            {
-                splice( @ls, $arg->{'IPP_MAX'} - scalar(@ls) );
-            }
-            push( @ls, "" );
-
-            $arg->{'IPP_NEXT'} = '';
-            $arg->{'IPP_THIS'} = '';
-        LS:
-            foreach (@ls)
-            {
-                next LS if ( m|/\.+$| or m|^\.+$| );
-
-                #   set IPP_PREV, IPP_THIS, IPP_NEXT
-                $arg->{'IPP_PREV'} = $arg->{'IPP_THIS'};
-                $arg->{'IPP_THIS'} = $arg->{'IPP_NEXT'};
-                $arg->{'IPP_NEXT'} = ( $_ eq '' ? '' : "$dirname/$_$ext" );
-                next LS if $arg->{'IPP_THIS'} eq '';
-
-                $out .=
-                    $self->ProcessFile( $mode, $_del, $arg->{'IPP_THIS'}, "",
-                    $level, $no_id, $arg );
-            }
-            delete $arg->{'IPP_NEXT'};
-            delete $arg->{'IPP_THIS'};
-            delete $arg->{'IPP_PREV'};
-        }
+        $self->_PatternProcess_helper(
+            $test,    \$out, $mode,  $_del,  $dirname,
+            $pattern, $ext,  $level, $no_id, $arg
+        );
     }
     return $out;
 }
