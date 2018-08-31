f9379edf22513787f5201e01c77f3e2b2f1d2116
Shlomi Fish <shlomif@shlomifish.org>
2018-09-01 00:18:39 +0300

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

---
+++ src/wml_include/WML_Backends/IPP/Line.pm
@@ -0,0 +1,237 @@
+##
+##  IPP -- Include Pre-Processor
+##  Copyright (c) 1997,1998,1999 Ralf S. Engelschall, All Rights Reserved.
+##  Copyright (c) 2000 Denis Barbier, All Rights Reserved.
+##
+
+package WML_Backends::IPP::Line;
+
+use 5.014;
+
+use strict;
+use warnings;
+
+use parent 'WML_Frontends::Wml::Base';
+
+use Class::XSAccessor (
+    constructor => 'new',
+    accessors   => +{
+        map { $_ => $_ }
+            qw(
+            l
+            self
+            )
+    },
+);
+
+use WML_Backends::IPP::Delimit ();
+
+sub _delim
+{
+    return WML_Backends::IPP::Delimit->new( delimiter => shift );
+}
+
+sub _line_Continuation_Support
+{
+    my ( $line, $l, $store ) = @_;
+
+    #   Line-Continuation Support
+    $$l =~ s|^\s+|| if $$store ne '';
+    return if $$l =~ m|^\\\s*\n$|;
+    if ( $$l =~ m|^(.*[^\\])\\\s*\n$| )
+    {
+        $$store .= $1;
+        return;
+    }
+    if ( $$l =~ m|^(.*\\)\\(\s*\n)$| )
+    {
+        $$l = $1 . $2;
+    }
+    $$l     = $$store . $$l;
+    $$store = '';
+    return 1;
+}
+
+sub _line_perform_Substitutions
+{
+    my ( $line, $l, $arg ) = @_;
+
+    #       Substitutions are performed from left to right and from
+    #       inner to outer, all operators have same precedence.
+    if ( $$l !~ m/(?:(?!\\).|^)\$\(([a-zA-Z0-9_]+)(?:(=|:[-=?+*])([^()]*))?\)/ )
+    {
+        return;
+    }
+    my ( $name, $op, $str ) = ( $1, $2, $3 );
+    if ( not defined($op) )
+    {
+        #   Normal Value
+        $$l =~
+s/((?!\\).|^)\$\($name\)/exists $arg->{$name} ? $1.$arg->{$name} : $1/e;
+        return 'redo';
+    }
+
+    my $subst = qr#(?:(?!\\).|^)\K\$\(\Q$name$op\E(?:[^()]*)\)#;
+
+    my $del = sub {
+        if ( $str eq '' )
+        {
+            delete $arg->{$name} if exists $arg->{$name};
+        }
+        else
+        {
+            $arg->{$name} = $str;
+        }
+    };
+
+    if ( $op eq '=' )
+    {
+        #   Assign
+        $$l =~ s/$subst//;
+        $del->();
+    }
+    elsif ( $op eq ':?' )
+    {
+        #   Indicate Error if Unset
+        $$l =~ s/$subst/exists $arg->{$name} ? $arg->{$name} : error($str)/e;
+    }
+    elsif ( $op eq ':-' )
+    {
+        #   Use Default Values
+        $$l =~ s/$subst/exists $arg->{$name} ? $arg->{$name} : $str/e;
+    }
+    elsif ( $op eq ':=' )
+    {
+        #   Use Default Values And Assign
+        $$l =~ s/$subst/exists $arg->{$name} ? $arg->{$name} : $str/e;
+        $del->();
+    }
+    elsif ( $op eq ':+' )
+    {
+        #   Use Alternative Value
+        $$l =~ s/$subst/exists $arg->{$name} ? $str : ''/e;
+    }
+    elsif ( $op eq ':*' )
+    {
+        #   Use Negative Alternative Value
+        $$l =~ s/$subst/exists $arg->{$name} ? '' : $str/e;
+    }
+    else
+    {
+        #   There is an error in these statements
+        die "Internal error when expanding variables";
+    }
+    return 'redo';
+}
+
+sub _process_line
+{
+    my ( $line, $l, $line_idx, $arg, $store, $level, $out, $fn, $realname ) =
+        @_;
+
+    #   EOL-comments
+    return if $$l =~ m/^\s*#(?!use|include|depends)/;
+
+    return if !$line->_line_Continuation_Support( $l, $store );
+
+    # Variable Interpolation
+    if ( my $ret = $line->_line_perform_Substitutions( $l, $arg ) )
+    {
+        return $ret;
+    }
+
+    #   EOL-comments again
+    return if $$l =~ m/^\s*#(?!use|include|depends)/;
+
+    #   Implicit Variables
+    $$l =~ s|__LINE__|$line_idx|g;
+    if ( $level == 0 and $arg->{'IPP_SRC_REALNAME'} ne '' )
+    {
+        $arg->{'IPP_SRC_REALNAME'} = './' . $arg->{'IPP_SRC_REALNAME'}
+            if $arg->{'IPP_SRC_REALNAME'} !~ m|/|;
+        $$l =~ s|__FILE__|$arg->{'IPP_SRC_REALNAME'}|g;
+    }
+    else
+    {
+        $$l =~ s|__FILE__|$fn|g;
+    }
+
+    #   remove one preceding backslash
+    $$l =~ s/\\(\$\([a-zA-Z0-9_]+(:[-=?+*][^()]*)?\))/$1/g;
+
+    if (
+        my $ret =
+        $line->_line_do_includes( $l, $arg, $out, $line_idx, $realname, $level,
+        )
+        )
+    {
+        return $ret;
+    }
+
+    return;
+}
+
+sub _line_do_includes
+{
+    my ( $line, $l, $arg, $out, $line_idx, $realname, $level, ) = @_;
+
+    #   ``#include'', ``#use'' and ``#depends'' directives
+    if ( my ( $cmd, $incfile, $args ) =
+        ( $$l =~ m/^#(use|include|depends)\s+(\S+)(.*)$/ ) )
+    {
+        #   set arguments
+        my %argO = %$arg;
+        WML_Backends::IPP::Args->new->setargs( $arg, $args );
+
+        #   do possible argument mapping
+        $incfile = $line->self->_map->mapfile($incfile);
+
+        my $type;
+
+        #   determine raw filename and type
+        if ( $incfile =~ m|^(\S+?)::(\S+)$| )
+        {
+            $type    = '<';
+            $incfile = "$2.$1";
+            $incfile =~ s|::|/|g;
+        }
+        elsif ( $incfile =~ m|^(['"<])([^'">]+)['">]$| )
+        {
+            $type    = $1;
+            $incfile = $2;
+        }
+        else
+        {
+            error("Unknown file-argument syntax: ``$incfile''");
+        }
+
+        #   now recurse down
+        $$out .=
+            $line->self->ProcessFile( $cmd, _delim($type),
+            $incfile, "", $level + 1, 0, $arg );
+        if ( not $line->self->opt_N and not $arg->{'IPP_NOSYNCLINES'} )
+        {
+            $$out .=
+                  "<__file__ $realname /><__line__ $line_idx />"
+                . "<protect pass=2><:# line $line_idx \"$realname\":></protect>\n";
+        }
+
+        #   reset arguments
+        %$arg = %argO;
+    }
+
+    #   ``__END__'' feature
+    elsif ( $$l =~ m|^\s*__END__\s*\n?$| )
+    {
+        return 'last';
+    }
+
+    #   plain text
+    else
+    {
+        $$out .= $$l;
+    }
+    return;
+}
+
+1;
