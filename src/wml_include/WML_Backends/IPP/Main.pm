bd16cd43808baf6696befad0e040f8521b99b561
Shlomi Fish <shlomif@shlomifish.org>
2018-09-01 01:48:36 +0300

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- src/wml_include/WML_Backends/IPP/Main.pm
+++ src/wml_include/WML_Backends/IPP/Main.pm
@@ -41,7 +41,7 @@ use Carp qw( cluck );
 use WML_Backends ();
 use WML_Backends::IPP::Args qw/ $IDENT_RE /;
 use WML_Backends::IPP::Delimit ();
-use WML_Backends::IPP::Line    ();
+use WML_Backends::IPP::File    ();
 use WML_Backends::IPP::Map     ();
 use WML_Frontends::Wml::Util qw/ canon_path /;
 
@@ -289,75 +289,8 @@ sub ProcessFile
 {
     my ( $self, $mode, $_del, $fn, $realname, $level, $no_id, $in_arg ) = @_;
 
-    my $arg = +{%$in_arg};
-
-    #   first check whether this is a filename pattern in which case
-    #   we must expand it
-    if ( my ( $dirname, $pattern, $ext ) =
-        ( $fn =~ m/^(.*?)(?=[?*\]])([?*]|\[[^\]]*\])(.*)$/ ) )
-    {
-        return $self->_expand_pattern( $dirname, $pattern, $ext, $mode,
-            $_del, $level, $no_id, $arg );
-    }
-    if ( not $self->_find_file( $_del, \$fn ) )
-    {
-        error("file not found: $fn");
-    }
-
-    #   stop if file was still included some time before
-    if ( not $no_id )
-    {
-        my $id = canon_path($fn);
-        if ( $mode eq 'use' )
-        {
-            return '' if ( exists $self->INCLUDES->{$id} );
-        }
-        $self->INCLUDES->{$id} = $_del->is_ang ? 1 : 2;
-    }
-
-    # Stop if just want to check dependency
-    return '' if $mode eq 'depends';
-
-    # Process the file
-    $realname = $fn if $realname eq '';
-    $self->verbose( $level, "|" );
-    $self->verbose( $level, "+-- $fn" );
-    my $in       = io()->file($fn);
-    my $line_idx = 0;
-    my $out      = '';
-    if ( not $self->opt_N and not $arg->{'IPP_NOSYNCLINES'} )
-    {
-        $out .=
-              "<__file__ $realname /><__line__ 0 />"
-            . "<protect pass=2><:# line $line_idx \"$realname\":></protect>\n";
-    }
-    my $store = '';
-
-LINES:
-    while ( my $l = $in->getline )
-    {
-        ++$line_idx;
-
-        my $op = WML_Backends::IPP::Line->new(
-            _main    => $self,
-            arg      => $arg,
-            l        => \$l,
-            line_idx => $line_idx,
-            out      => \$out,
-            realname => $realname,
-        )->_process_line( \$store, $level, $fn, ) // '';
-        if ( $op eq 'last' )
-        {
-            last LINES;
-        }
-        elsif ( $op eq 'redo' )
-        {
-            redo LINES;
-        }
-    }
-    $out .= $store;
-
-    return $out;
+    return WML_Backends::IPP::File->new( _main => $self )
+        ->ProcessFile( $mode, $_del, $fn, $realname, $level, $no_id, $in_arg );
 }
 
 sub _write_includes
