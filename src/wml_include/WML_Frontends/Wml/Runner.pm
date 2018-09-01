5d706d5a9faf0a3e1717462dcb180ed570092b36
Shlomi Fish <shlomif@shlomifish.org>
2018-09-01 14:30:53 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- src/wml_include/WML_Frontends/Wml/Runner.pm
+++ src/wml_include/WML_Frontends/Wml/Runner.pm
@@ -7,27 +7,6 @@
 ##  the Free Software Foundation; either version 2 of the License, or
 ##  (at your option) any later version.
 ##
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License for more details.
-##
-##  You should have received a copy of the GNU General Public License
-##  along with this program; if not, write to
-##
-##      Free Software Foundation, Inc.
-##      59 Temple Place - Suite 330
-##      Boston, MA  02111-1307, USA
-##
-##  Notice, that ``free software'' addresses the fact that this program
-##  is __distributed__ under the term of the GNU General Public License
-##  and because of this, it can be redistributed and modified under the
-##  conditions of this license, but the software remains __copyrighted__
-##  by the author. Don't intermix this with the general meaning of
-##  Public Domain software or such a derivated distribution label.
-##
-##  The author reserves the right to distribute following releases of
-##  this program under different conditions or license agreements.
 
 package WML_Frontends::Wml::Runner;
 
@@ -1054,7 +1033,3 @@ sub run_with_ARGV
 }
 
 1;
-
-__END__
-
-# vim: ft=perl
