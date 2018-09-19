3aa8a11ecf4fcb9bac2032c59109cdab967cae2d
Shlomi Fish <shlomif@shlomifish.org>
2018-09-19 17:02:33 +0300

    appveyor failure isolate/trim ; merge

--- src/wml_backend/p3_eperl/eperl_main.c
+++ src/wml_backend/p3_eperl/eperl_main.c
@@ -1,3 +1,205 @@
+#include "eperl_config.h"
+#include "eperl_global.h"
+#include "eperl_perl5.h"
+#include "eperl_perl5_sm.h"
+#include "eperl_proto.h"
+
+#ifdef HAVE_PERL_DYNALOADER
+
+extern void boot_DynaLoader _((pTHX_ CV* cv));
+
+/*
+**
+**  the Perl XS init function for dynamic library loading
+**
+*/
+void Perl5_XSInit(pTHX)
+{
+   char *file = __FILE__;
+   /* dXSUB_SYS; */
+   /* dummy = 0; */ /* make gcc -Wall happy ;-) */
+
+   /* do newXS() the available modules */
+   DO_NEWXS_STATIC_MODULES
+}
+#endif /* HAVE_PERL_DYNALOADER */
+
+/*
+**
+**  sets a Perl scalar variable
+**
+*/
+void Perl5_SetScalar(pTHX_ char *pname, char *vname, char *vvalue)
+{
+    dTHR;
+    ENTER;
+    save_hptr(&PL_curstash);
+    PL_curstash = gv_stashpv(pname, TRUE);
+    sv_setpv(perl_get_sv(vname, TRUE), vvalue);
+    LEAVE;
+    return;
+}
+
+/*
+**
+**  remember a Perl scalar variable
+**  and set it later
+**
+**  (this is needed because we have to
+**   remember the scalars when parsing
+**   the command line, but actually setting
+**   them can only be done later when the
+**   Perl 5 interpreter is allocated !!)
+**
+*/
+
+char *Perl5_RememberedScalars[1024] = { NULL };
+
+void Perl5_RememberScalar(char *str)
+{
+    int i;
+
+    for (i = 0; Perl5_RememberedScalars[i] != NULL; i++)
+        ;
+    Perl5_RememberedScalars[i++] = strdup(str);
+    Perl5_RememberedScalars[i++] = NULL;
+    return;
+}
+
+void Perl5_SetRememberedScalars(pTHX)
+{
+    char ca[1024];
+    char *cp;
+    int i;
+
+    for (i = 0; Perl5_RememberedScalars[i] != NULL; i++) {
+        strncpy(ca, Perl5_RememberedScalars[i], 1023);
+        ca[1023] = NUL;
+        cp = strchr(ca, '=');
+        if (cp != NULL)
+            *cp++ = '\0';
+        else
+            cp = "";
+        Perl5_SetScalar(aTHX_ "main", ca, cp);
+    }
+}
+
+int Perl5_Run(int myargc, char **myargv, int mode, int fCheck, int keepcwd, char *source, char **env, char *perlscript, char *perlstderr, char *perlstdout)
+{
+    int rc;
+    FILE *er;
+    FILE *out;
+    char *cpBuf = NULL;
+    char sourcedir[2048];
+    char *cp;
+    static PerlInterpreter *my_perl = NULL;
+    struct stat st;
+    int size;
+    char cwd[MAXPATHLEN];
+
+    /* open a file for Perl's STDOUT channel
+       and redirect stdout to the new channel */
+    if ((out = fopen(perlstdout, "w")) == NULL) {
+        fprintf(stderr, "Cannot open STDOUT file `%s' for writing", perlstdout);
+        CU(mode == 0 ? EX_IOERR : EX_OK);
+    }
+
+    /* open a file for Perl's STDERR channel
+       and redirect stderr to the new channel */
+    if ((er = fopen(perlstderr, "w")) == NULL) {
+        fprintf(stderr, "Cannot open STDERR file `%s' for writing", perlstderr);
+        CU(mode == 0 ? EX_IOERR : EX_OK);
+    }
+
+    my_perl = perl_alloc();
+    perl_construct(my_perl);
+    perl_init_i18nl10n(1);
+
+    /*  now parse the script!
+        NOTICE: At this point, the script gets
+        only _parsed_, not evaluated/executed!  */
+#ifdef HAVE_PERL_DYNALOADER
+    rc = perl_parse(my_perl, Perl5_XSInit, myargc, myargv, env);
+#else
+    rc = perl_parse(my_perl, NULL, myargc, myargv, env);
+#endif
+    if (rc != 0) {
+        if (fCheck && mode == 0) {
+            fclose(er); er = NULL;
+            CU(EX_FAIL);
+        }
+        else {
+            fclose(er); er = NULL;
+            fprintf(stderr, "Perl parsing error (interpreter rc=%d) error=%s", rc, SvTRUE(ERRSV) ? SvPV_nolen(ERRSV) : "");
+            CU(mode == 0 ? EX_FAIL : EX_OK);
+        }
+    }
+
+    /* Stop when we are just doing a syntax check */
+    if (fCheck && mode == 0) {
+        fclose(er); er = NULL;
+        fprintf(stderr, "%s syntax OK\n", source);
+        CU(-1);
+    }
+
+    /* change to directory of script:
+       this actually is not important to us, but really useful
+       for the ePerl source file programmer!! */
+    cwd[0] = NUL;
+    if (!keepcwd) {
+        /* if running as a Unix filter remember the cwd for outputfile */
+        if (mode == 0)
+        {
+            if (! getcwd(cwd, MAXPATHLEN))
+            {
+                CU(-1);
+            }
+        }
+        /* determine dir of source file and switch to it */
+        strncpy(sourcedir, source, sizeof(sourcedir));
+        sourcedir[sizeof(sourcedir)-1] = NUL;
+        for (cp = sourcedir+strlen(sourcedir); cp > sourcedir && *cp != '/'; cp--)
+            ;
+        *cp = NUL;
+        if (chdir(sourcedir) != 0) {
+            CU(-1);
+        }
+    }
+
+    /*  Set the previously remembered Perl 5 scalars (option -d) */
+    Perl5_SetRememberedScalars(aTHX);
+
+    /*  NOW IT IS TIME to evaluate/execute the script!!! */
+    rc = perl_run(my_perl);
+
+    /*  pre-close the handles, to be able to check
+        its size and to be able to display the contents */
+    fclose(out); out = NULL;
+    fclose(er);  er  = NULL;
+
+    /*  when the Perl interpreter failed or there
+        is data on stderr, we print a error page */
+    if (stat(perlstderr, &st) == 0)
+        size = st.st_size;
+    else
+        size = 0;
+    if (rc != 0 || size > 0) {
+        fprintf(stderr, "Perl runtime error (interpreter rc=%d)", rc);
+        CU(mode == 0 ? EX_FAIL : EX_OK);
+    }
+
+    CUS: /* the Clean Up Sequence */
+
+    /* Ok, the script got evaluated. Now we can destroy
+       and de-allocate the Perl interpreter */
+    if (my_perl) {
+       perl_destruct(my_perl);
+       perl_free(my_perl);
+    }
+    return rc;
+}
+
+/*EOF*/
 /*
 **        ____           _
 **    ___|  _ \ ___ _ __| |
