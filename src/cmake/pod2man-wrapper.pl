073b0ff727bb14357a02ec4132287aa6ef1eed38
Shlomi Fish <shlomif@shlomifish.org>
2019-01-01 23:21:04 +0200

    Fix the formatting and make imports explicit.
    Test for it.

--- src/cmake/pod2man-wrapper.pl
+++ src/cmake/pod2man-wrapper.pl
@@ -3,77 +3,75 @@
 use strict;
 use warnings;
 
-use Getopt::Long;
-use File::Temp qw/tempdir/;
-use File::Copy;
-use File::Basename;
+use Getopt::Long qw/ GetOptions /;
+use File::Temp qw/ tempdir /;
+use File::Copy qw/ copy /;
+use File::Basename qw/ basename /;
 
-my ($src, $dest, $sect, $center, $release);
+my ( $src, $dest, $sect, $center, $release );
 
 GetOptions(
-    'src=s' => \$src,
-    'dest=s' => \$dest,
+    'src=s'     => \$src,
+    'dest=s'    => \$dest,
     'section=s' => \$sect,
-    'center=s' => \$center,
+    'center=s'  => \$center,
     'release=s' => \$release,
-)
-    or die "Wrong options.";
+) or die "Wrong options.";
 
-if (!defined($src))
+if ( !defined($src) )
 {
     die "src not specified.";
 }
 
-if (!defined($dest))
+if ( !defined($dest) )
 {
     die "dest not specified.";
 }
 
-if (!defined($sect))
+if ( !defined($sect) )
 {
     die "section not specified.";
 }
 
-if (!defined($center))
+if ( !defined($center) )
 {
     die "center not specified.";
 }
 
-if (!defined($release))
+if ( !defined($release) )
 {
     die "release not specified.";
 }
 
-my $dir = tempdir( CLEANUP => 1);
+my $dir = tempdir( CLEANUP => 1 );
 
 my $pod;
-if ($src =~ m(/wml_include/)) {
+if ( $src =~ m(/wml_include/) )
+{
     $pod = $src;
     $pod =~ s(^.*/wml_include/)(wml::);
     $pod =~ s(/)(::)g;
     $pod =~ s(\.(src|pl|pm|pod)$)();
-} else {
-    $pod = basename($src, '.src', '.pl', '.pm', '.pod');
+}
+else
+{
+    $pod = basename( $src, '.src', '.pl', '.pm', '.pod' );
 }
 $pod = "$pod.pod";
 
-if (! -e $src)
+if ( !-e $src )
 {
     die "Cannot find '$src'";
 }
-copy($src, "$dir/$pod");
+copy( $src, "$dir/$pod" );
 chdir($dir);
 
-if(
-system(
-    'pod2man',
-    "--section=$sect",
-    "--center=$center",
-    "--release=$release",
-    $pod,
-    $dest
-)
-)
+if (
+    system(
+        'pod2man',            "--section=$sect", "--center=$center",
+        "--release=$release", $pod,              $dest
+    )
+    )
 {
     die "pod2man failed! $!";
 }
